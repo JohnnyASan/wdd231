f37907c59f55c4ba4ddc14b67bdee72ee3829f33
JohnnyASan <johnanthonysanabria@gmail.com>
2024-06-24 11:56:21 -0600

    got the basic grid showing directory members

--- chamber/scripts/common.js
+++ chamber/scripts/common.js
@@ -1,3 +1,24 @@
+const footer = document.querySelector('#footer');
+footer.innerHTML = 
+`<div id="contactInfo" class="footerCell">
+    <p id="contactName">Naples UT Chamber of Commerce</p>
+    <p id="contactAddress1">123 Sesame St</p>
+    <p id="contactAddress2">Naples, UT 84078</p>
+    <p id="contactEmail">contact@napleschamber.com</p>
+    <p id="contactPhone"></p>
+</div>
+<div id="socialMedia" class="footerCell">
+    <a href="https://youtube.com"><img src="./images/youtube.png" alt="YouTube logo by riajulislam"></a>
+    <a href="https://facebook.com"><img src="./images/facebook.png" alt="Facebook logo by riajulislam"></a>
+    <a href="https://linkedin.com"><img src="./images/linkedin.png" alt="LinkedIn logo by riajulislam"></a>
+</div>
+<div id="devDetails" class="footerCell">
+    <p>WDD231 Class Project</p>
+    <p>Johnny Sanabria</p>
+    <p id="currentYear"> Naples UT Chamber of Commerce</p>
+    <p id="lastModified"></p>
+</div>`;
+
 const hamburgerElement = document.querySelector('#navButton');
 const navElement = document.querySelector('#navElement');
 
@@ -5,3 +26,11 @@ hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 });
+
+const lastModified = document.getElementById('lastModified');
+lastModified.innerHTML = `Last Updated on <i>${document.lastModified}</i>`;
+
+
+const currentYear = document.getElementById('currentYear');
+currentYear.textContent = "@ " + new Date().getUTCFullYear() + " Naples UT Chamber of Commerce";
+
--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,2 +1,29 @@
 import('./common.js');
 
+const membersList = document.querySelector('#membersList');
+
+async function displayMembers() {
+    const membersResponse = await fetch('./data/members.json');
+    const members = await membersResponse.json();
+    // console.log(members);
+
+    members.forEach(m => {
+        console.log(m);
+        const card = document.createElement('div');
+        card.classList.add('memberCard');
+
+        const fig = document.createElement('figure');
+        const logo = document.createElement('img');
+        const caption = document.createElement('figcaption');
+        caption.innerText = `${m.name}`;
+        logo.src = m.imageUrl;
+
+        fig.appendChild(logo);
+        fig.appendChild(caption);
+        card.appendChild(fig);
+        membersList.appendChild(card);
+    });
+}
+
+displayMembers();
+
