084edee40530d12b6d061c467cbaf01f6c007988
JohnnyASan <johnanthonysanabria@gmail.com>
2024-06-24 13:32:07 -0600

    Got everything looking decent

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,22 @@
 import('./common.js');
 
 const membersList = document.querySelector('#membersList');
+const oneColButton = document.querySelector('#oneColButton');
+const threeColButton = document.querySelector('#threeColButton');
+
+oneColButton.addEventListener('click', () => {
+    oneColButton.classList.toggle('selected');
+    threeColButton.classList.toggle('selected');
+    membersList.classList.remove('threeColGrid');
+    membersList.classList.add('oneColGrid');
+});
+
+threeColButton.addEventListener('click', () => {
+    oneColButton.classList.toggle('selected');
+    threeColButton.classList.toggle('selected');
+    membersList.classList.remove('oneColGrid');
+    membersList.classList.add('threeColGrid');
+});
 
 async function displayMembers() {
     const membersResponse = await fetch('./data/members.json');
@@ -15,7 +31,14 @@ async function displayMembers() {
         const fig = document.createElement('figure');
         const logo = document.createElement('img');
         const caption = document.createElement('figcaption');
-        caption.innerText = `${m.name}`;
+        caption.innerHTML = 
+        `
+        <h3>${m.name}</h3>
+        <p>${m.phone}</p>
+        <p>${m.address1}</p>
+        <p>${m.address2}</p>
+        <a href="${m.website}">Website</a>
+        `;
         logo.src = m.imageUrl;
 
         fig.appendChild(logo);
