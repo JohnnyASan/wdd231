0229c4ede6068d2c4d9c487858feb02432990812
JohnnyASan <johnanthonysanabria@gmail.com>
2024-07-02 16:20:56 -0600

    Added a modal and improved styling

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,2 +1,52 @@
 import('./common.js');
 
+const membershipHelp = document.querySelector('#levelHelp');
+membershipHelp.addEventListener('click', () => {
+    displayMembershipModal();
+});
+
+const membershipLevelModal = document.querySelector('#membershipLevelModal');
+
+membershipLevelModal.addEventListener('click', (event) => {
+    if (event.target.id !== 'dialogDiv') {
+        membershipLevelModal.close();
+    }
+});
+
+function displayMembershipModal() {
+    membershipLevelModal.innerHTML = `
+    <table>
+        <tr>
+            <th>Membership Level</th>
+            <th>Description</th>
+            <th>Monthly Fee</th>
+        </tr>
+        <tr>
+            <td>Bronze</td>
+            <td>Bronze Membership Description</td>
+            <td>$35</td>
+        </tr>
+        <tr>
+            <td>Silver</td>
+            <td>Silver Membership Description</td>
+            <td>$75</td>
+        </tr>
+        <tr>
+            <td>Gold</td>
+            <td>Gold Membership Description</td>
+            <td>$125</td>
+        </tr>
+        <tr>
+            <td>Non-Profit</td>
+            <td>Non-Profit Membership Description</td>
+            <td>$0</td>
+        </tr>
+    </table>
+    `;
+
+    membershipLevelModal.showModal();
+
+    closeModal.addEventListener('click', () => {
+        membershipLevelModal.close();
+    })
+}
