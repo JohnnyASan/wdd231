488bc3de6a8b551d8ce26eb5d5235f8ec9b5161d
JohnnyASan <johnanthonysanabria@gmail.com>
2024-07-13 22:42:14 -0600

    started discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,2 +1,24 @@
 import('./common.js');
+import('./calendar.js')
 
+const calendar = document.querySelector('#sideBarItem3');
+calendar.innerHTML = `
+    <div class="calendar">
+        <header class="calendar__header">
+            <div class="calendar__month"></div>
+            <div class="calendar__year"></div>
+        </header>
+        <div class="calendar__grid">
+            <div class="calendar__day-names">
+            <span class="calendar__day-name">S</span>
+            <span class="calendar__day-name">M</span>
+            <span class="calendar__day-name">T</span>
+            <span class="calendar__day-name">W</span>
+            <span class="calendar__day-name">T</span>
+            <span class="calendar__day-name">F</span>
+            <span class="calendar__day-name">S</span>
+            </div>
+            <div class="calendar__day-numbers"></div>
+        </div>
+    </div>
+`;
