84a6975d6185d48e8cb0dcfc31609cfa9e48b8bf
JohnnyASan <johnanthonysanabria@gmail.com>
2024-07-13 20:03:46 -0600

    Did siteplan

---
+++ sourdough/scripts/recipeAPI.js
@@ -0,0 +1,6 @@
+const appId = 'e716f855'
+const appKey = '20a20f6cb6c9c91a8e4c4cf49eec1f8b';
+const baseURL = 'api.edamam.com';
+const recipePath = '/api/recipes/v2';
+// https://developer.edamam.com/edamam-docs-recipe-api
+
